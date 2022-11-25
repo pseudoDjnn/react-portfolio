f0bbb02dbbcfc89a7629998bf30d79169fc878ee
p5eudoDjnn <alexander.havers@gmail.com>
2022-11-25 14:18:20 -0500

    added Tailwind CSS and cleared out some boilerplate

--- src/App.js
+++ src/App.js
@@ -1,23 +1,9 @@
-import logo from './logo.svg';
-import './App.css';
-
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container mx-auto bg-gray-200 rounded-xl shadow-md">
+      <p className="text-3xl text-gray-700 fornt-bold mb-5"> Welcome</p>
+      <h1>Hello World</h1>
+      <p className="text-gray-500 text-lg">This should work!</p>
     </div>
   );
 }
