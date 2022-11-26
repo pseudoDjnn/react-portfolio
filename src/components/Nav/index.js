f50cc65026bcc6a81ec12474caef6e29311fde76
p5eudoDjnn <alexander.havers@gmail.com>
2022-11-26 15:39:20 -0500

    used && on useState truthy/falsy so navigation can be used for different media

--- src/components/Nav/index.js
+++ src/components/Nav/index.js
@@ -45,6 +45,19 @@ const NavBar = () => {
       >
         {donut ? <FaTimes size={30} /> : <FaBars size={30} />}
       </div>
+
+      {donut && (
+        <ul className="flex flex-col justify-center items-center absolute top-0 left-0 w-full h-screen bg-gradient-to-b from-black to-slate-800 text-gray-500">
+          {links.map(({ id, link }) => (
+            <li
+              key={id}
+              className="px-4 cursor-pointer capitalize py-6 text-4xl"
+            >
+              {link}
+            </li>
+          ))}
+        </ul>
+      )}
     </div>
   );
 };
