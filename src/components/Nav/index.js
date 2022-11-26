6132e351bc314f4eb5420792279aa87cc26f5314
p5eudoDjnn <alexander.havers@gmail.com>
2022-11-26 15:05:42 -0500

    created useState for react icons and changed color scheme

--- src/components/Nav/index.js
+++ src/components/Nav/index.js
@@ -1,7 +1,10 @@
-import React from "react";
+import React, { useState } from "react";
 import { FaBars, FaTimes } from "react-icons/fa";
 
 const NavBar = () => {
+  // useState for navigation bar
+  const [donut, setDonut] = useState(false);
+
   const links = [
     {
       id: 1,
@@ -22,20 +25,26 @@ const NavBar = () => {
   ];
 
   return (
-    <div className="flex justify-between items-center w-full h-20 text-white bg-black px-4 fixed">
+    <div className="flex justify-between items-center w-full h-20 text-orange-700 bg-slate-800 px-4 fixed">
       <div>
         <h1 className="text-5xl font-signature ml-2">Alexander Havers</h1>
       </div>
-      <ul className="flex">
+      <ul className="hidden md:flex">
         {links.map(({ id, link }) => (
           <li
             key={id}
-            className="px-4 cursor-pointer capitalize font-medium text-stone-500 hover:scale-105 duration-200"
+            className="px-4 cursor-pointer capitalize font-medium text-orange-700 hover:scale-105 duration-200"
           >
             {link}
           </li>
         ))}
       </ul>
+      <div
+        onClick={() => setDonut(!donut)}
+        className="cursor-pointer pr-4 z-10 text-orange-700"
+      >
+        {donut ? <FaTimes size={30} /> : <FaBars size={30} />}
+      </div>
     </div>
   );
 };
