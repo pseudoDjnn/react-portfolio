9917a1d4801a5b2fbd5726d8e2437cae54dd3e30
p5eudoDjnn <alexander.havers@gmail.com>
2022-11-27 14:31:54 -0500

    mapped array for conditional render to work for my portfolio

--- src/components/Project/index.js
+++ src/components/Project/index.js
@@ -48,22 +48,25 @@ const Portfolio = () => {
             A listing of all current projects for my 2022 portfolio.
           </p>
         </div>
+
         <div className="grid sm:grid-cols-2 md:grid-cols-3 gap-8 px-12 sm:px-0">
-          <div className="shadow-md shadow-slate-600 rounded-lg">
-            <img
-              src={JATE}
-              alt="demo"
-              className="rounded-md duration-700 hover:scale-105"
-            />
-            <div className="flex items-center justify-center">
-              <button className="w-1/2 px-6 py-2 m-4 duration-300 hover:scale-105">
-                Demo
-              </button>
-              <button className="w-1/2 px-6 py-2 m-4 duration-300 hover:scale-105">
-                Code
-              </button>
+          {projectPortfolio.map(({ id, src }) => (
+            <div key={id} className="shadow-md shadow-slate-600 rounded-lg">
+              <img
+                src={src}
+                alt="demo"
+                className="rounded-md duration-700 hover:scale-105"
+              />
+              <div className="flex items-center justify-center">
+                <button className="w-1/2 px-6 py-2 m-4 duration-300 hover:scale-105">
+                  Demo
+                </button>
+                <button className="w-1/2 px-6 py-2 m-4 duration-300 hover:scale-105">
+                  Code
+                </button>
+              </div>
             </div>
-          </div>
+          ))}
         </div>
       </div>
     </div>
