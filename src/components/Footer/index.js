ae169e01fc4d802635ee1b43742e86c0905488ce
p5eudoDjnn <alexander.havers@gmail.com>
2022-11-27 10:26:52 -0500

    fixed spacing in className for bg-color to show

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -4,18 +4,72 @@ import { HiOutlineMail } from "react-icons/hi";
 import { BsFillPersonLinesFill } from "react-icons/bs";
 
 const Footer = () => {
+  // Array for links to render conditionally
+  const links = [
+    {
+      id: 1,
+      child: (
+        <>
+          Linkedin <FaLinkedin size={32} />
+        </>
+      ),
+      href: "https://www.linkedin.com/in/alexanderhavers/",
+      style: "rounded-tr-md",
+    },
+    {
+      id: 2,
+      child: (
+        <>
+          GitHub <FaGithub size={32} />
+        </>
+      ),
+      href: "https://github.com/pseudoDjnn",
+    },
+    {
+      id: 3,
+      child: (
+        <>
+          Mail <HiOutlineMail size={32} />
+        </>
+      ),
+      href: "mailto:alexander.havers@gmail.com",
+    },
+    {
+      id: 4,
+      child: (
+        <>
+          Resume <BsFillPersonLinesFill size={32} />
+        </>
+      ),
+      href: "/resume 22.pdf",
+      style: "rounded-br-md",
+      download: true,
+    },
+  ];
+
   return (
-    <div className="flex flex-col top-[35%] left-0 fixed">
+    <div className="hidden lg:flex flex-col top-[75%] fixed">
       <ul>
-        <li className="flex justify-between items-center w-40 h-14 px-4 ml-[-100px] hover:ml-[-10px] hover:rounded-md duration-500">
-          <a
-            href=""
-            className="flex justify-between items-center w-full text-slate-300"
+        {links.map(({ id, child, href, style, download }) => (
+          <li
+            key={id}
+            className={
+              "flex justify-between items-center w-40 h-14 px-4 ml-[-100px] hover:ml-[-10px] hover:rounded-md duration-500 bg-zinc-900" +
+              " " +
+              style
+            }
           >
-            Linkedin
-            <FaLinkedin size={32} />
-          </a>
-        </li>
+            <a
+              href={href}
+              className="flex justify-between items-center w-full text-orange-600"
+              download={download}
+              target="_blank"
+              rel="noreferrer"
+            >
+              {child}
+            </a>
+          </li>
+        ))}
       </ul>
     </div>
   );
