19432fed7a9987020d7ce001e1860362b081e5a4
p5eudoDjnn <alexander.havers@gmail.com>
2022-11-28 14:58:46 -0500

    added utils to contact component so modal thorws error message when form needs to be filled out

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import Header from "./components/Header";
 import NavBar from "./components/Nav";
 import About from "./components/About";
@@ -6,11 +6,20 @@ import Project from "./components/Project";
 import Experience from "./components/Experience";
 import Contact from "./components/Contact";
 import Footer from "./components/Footer";
+import { capitalizeFirstLetter } from "./utils/helpers";
 
 function App() {
+  const [currentSelect, setCurrentSelect] = useState("About Me");
+
+  useEffect(() => {
+    document.title = capitalizeFirstLetter(currentSelect);
+  }, [currentSelect]);
   return (
     <div>
-      <NavBar />
+      <NavBar
+        currentSelect={currentSelect}
+        setCurrentSelect={setCurrentSelect}
+      />
       <Header />
       <About />
       <Project />
--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -1,6 +1,42 @@
-import React from "react";
+import React, { useState } from "react";
+import { validateEmail, validateName } from "../../utils/helpers";
 
 const Contact = () => {
+  const [formState, setFormState] = useState({
+    name: "",
+    email: "",
+    message: "",
+  });
+  const { name, email, message } = formState;
+  const [errorMessage, setErrorMessage] = useState("");
+  function handleChange(e) {
+    if (e.target.name === "name") {
+      const isValid = validateName(e.target.value);
+      if (!isValid) {
+        setErrorMessage("Please enter a valid name");
+      }
+    } else if (e.target.name === "email") {
+      const isValid = validateEmail(e.target.value);
+      if (!isValid) {
+        setErrorMessage("Please enter a valid email.");
+      } else {
+        setErrorMessage("");
+      }
+    } else if (e.target.name === "message") {
+      if (!e.target.value) {
+        setErrorMessage("Message is required.");
+      } else {
+        setErrorMessage("");
+      }
+    }
+    if (!errorMessage) {
+      setFormState({ ...formState, [e.target.name]: e.target.value });
+    }
+  }
+  function handleSubmit(e) {
+    e.preventDefault();
+    console.log(formState);
+  }
   return (
     <div
       name="contact"
@@ -14,24 +50,43 @@ const Contact = () => {
           <p className="py-6">Submit the form and get in touch!</p>
         </div>
         <div>
-          <form action="" className="flex flex-col w-full md:w-1/2">
+          <form
+            onSubmit={handleSubmit}
+            action=""
+            className="flex flex-col w-full md:w-1/2"
+          >
             <input
               type="text"
               name="name"
+              defaultValue={name}
+              onBlur={handleChange}
               placeholder="Enter your name"
               className="p-2 bg-transparent border-2 rounded-md text-orange-100 focus:outline-none"
+              required
             />
             <input
               type="text"
               name="email"
+              defaultValue={email}
+              onBlur={handleChange}
               placeholder="Enter your Email"
               className="p-2 bg-transparent border-2 rounded-md text-orange-100 focus:outline-none my-4"
+              required
             />
             <textarea
+              type="text"
               name="message"
+              defaultValue={message}
+              onBlur={handleChange}
               rows="10"
               className="p-2 bg-transparent border-2 rounded-md text-orange-100 focus:outline-none"
-            ></textarea>
+              required
+            />
+            {errorMessage && (
+              <div className="pb-6">
+                <p className="error-text">{errorMessage}</p>
+              </div>
+            )}
             <button className="text-orange-100 bg-gradient-to-br from-slate-800 to-orange-800 px-6 py-3 my-8 mx-auto flex items-center rounded-md hover:scale-110 duration-500">
               Reach out
             </button>
