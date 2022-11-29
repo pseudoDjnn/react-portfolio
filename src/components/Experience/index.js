ed5acd00609ce56f93b76de7fa8ed6e1abc90cbc
p5eudoDjnn <alexander.havers@gmail.com>
2022-11-29 14:22:18 -0500

    added css for scroll bar sleak design

--- src/components/Experience/index.js
+++ src/components/Experience/index.js
@@ -58,7 +58,7 @@ const Experience = () => {
           <p className="text-4xl font-bold border-b-4 border-orange-800 p-2 inline">
             Experience
           </p>
-          <p className="py-6"> Ther are the technologies I have worked with</p>
+          <p className="py-6"> There are the technologies I have worked with</p>
         </div>
         <div className="w-full grid grid-cols-2 sm:grid-cols-3 gap-8 text-center py-8 px-12 sm:px-0">
           {techno.map(({ id, src, title, style }) => (
